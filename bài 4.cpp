88f648026ff883fc350ddf8ea6c4a33ee646eb41
An256122006 <dinhtrongan2006@gmail.com>
2024-11-17 16:54:29 +0700

    Add files via upload

---
+++ bài 4.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int a; 
+	printf ("moi nhap gia tri a:");
+	scanf ("%d" , &a);
+	if (a>5)
+	{
+		printf ("%d dat",a); 
+	 } 
+	 else
+	  {
+	 printf ("%d khong dat",a); 	
+	 }
+	 return 0; 
+ } 
