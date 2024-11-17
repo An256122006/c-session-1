e7a1fb810e7c7ab65346b09d272f7008c6833395
An256122006 <dinhtrongan2006@gmail.com>
2024-11-17 16:28:06 +0700

    Add files via upload

---
+++ bài 1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int a, b; 
+	printf ("moi nhap gia tri a:");
+	scanf ("%d" , &a);
+	printf  ("moi nhap gia tri b :");
+	scanf ("%d" , &b); 
+	if (a > b)
+	{
+	printf ("so lon nhat là %d" , a);
+}
+else {
+printf ("so lon nhat la %d" , b);	
+} 
+
+	return 0; 
+ } 
